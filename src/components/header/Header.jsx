5bda87bca560d9b81e312c70e912efb960744c86
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-06-16 18:04:52 +0530

    Header components

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import './header.css'
+import React from "react";
+import CTA from "./CTA";
+import "./header.css";
+import ME from "../../assets/me1.jpg";
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+	return (
+		<header>
+			<div className='container header__container'>
+				<h5>Hello I'm</h5>
+				<h1>Sagar Maheshwari</h1>
+				<h5 className='text-light'>Fullstack Developer</h5>
+				<CTA />
 
-export default Header
+				<div className='me'>
+					<img src={ME} alt='Sagar Maheshwari' />
+				</div>
+
+				<a href='#contact' className='scroll__down'>
+					Scroll Down
+				</a>
+			</div>
+		</header>
+	);
+};
+
+export default Header;
---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const HeaderSocials = () => {
+	return (
+		<div className='header__socials'>
+			<a href='https://github.com/SagarM21' target='_blank'></a>
+			<a
+				href='https://www.linkedin.com/in/sagar-maheshwari-3330b6166/'
+				target='_blank'
+			></a>
+		</div>
+	);
+};
+
+export default HeaderSocials;
