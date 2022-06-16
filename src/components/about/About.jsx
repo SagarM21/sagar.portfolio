8b2f50d36653d801d34d582d5fb0e08dde3c59e0
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-06-16 19:13:43 +0530

    About JSX

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,49 @@
-import React from 'react'
-import './about.css'
+import React from "react";
+import ME from "../../assets/me-about.jpg";
+import { FaAward } from "react-icons/fa";
+import { VscFolderLibrary } from "react-icons/vsc";
+import "./about.css";
 
 const About = () => {
-  return (
-    <section id='about'>About</section>
-  )
-}
+	return (
+		<section id='about'>
+			<h5>Get To Know</h5>
+			<h2>About Me</h2>
+			<div className='container about__container'>
+				<div className='about__me'>
+					<div className='about__image-me'>
+						<img src={ME} alt='About image' />
+					</div>
+				</div>
+				<div className='about__content'>
+					<div className='about__cards'>
+						<article className='about__card'>
+							<FaAward className='about__icon' />
+							<h5>Experience</h5>
+							<small>3 Months Working</small>
+						</article>
 
-export default About
+						<article className='about__card'>
+							<VscFolderLibrary className='about__icon' />
+							<h5>Projects</h5>
+							<small>10+ Completed Projects</small>
+						</article>
+					</div>
+
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis
+						autem vero rerum distinctio recusandae dolore animi sed, quidem
+						velit voluptates at sit inventore culpa laborum repellat tempora
+						libero excepturi dolores.
+					</p>
+
+					<a href='#contact' className='btn  btn-primary'>
+						Let's Talk
+					</a>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default About;
