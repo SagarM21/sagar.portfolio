167da16e738ae4463bc973d2d92b43bb3b9cbd85
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-06-17 17:29:23 +0530

    Footer Component with designing

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,52 @@
-import React from 'react'
-import './footer.css'
+import React from "react";
+import { BsTwitter } from "react-icons/bs";
+import { BsInstagram } from "react-icons/bs";
+import "./footer.css";
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+	return (
+		<footer>
+			<a href='#' className='footer__logo'>
+				SAGAR
+			</a>
 
-export default Footer
+			<ul className='permalinks'>
+				<li>
+					<a href='#'>Home</a>
+				</li>
+				<li>
+					<a href='#about'>About</a>
+				</li>
+				<li>
+					<a href='#experience'>Experience</a>
+				</li>
+				<li>
+					<a href='#portfolio'>Portfolio</a>
+				</li>
+				<li>
+					<a href='#contact'>Contact</a>
+				</li>
+			</ul>
+
+			<div className='footer__socials'>
+				<a href='https://twitter.com/Sagar_Maheshh' target='_blank'>
+					<BsTwitter />
+				</a>
+
+				<a
+					href='https://www.instagram.com/_sagar_maheshwari/?hl=en'
+					target='_blank'
+				>
+					{" "}
+					<BsInstagram />
+				</a>
+			</div>
+
+			<div className='footer__copyright'>
+				<small>Made with ❤️</small>
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
