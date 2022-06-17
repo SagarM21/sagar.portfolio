5c1c9ff2e0e7d175ba37901fcf40219fa392ffbc
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-06-17 13:19:52 +0530

    Portfolio Component

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,7 +4,53 @@ import dev from "../../assets/dev.png";
 import jobify from "../../assets/jobify.png";
 import memories from "../../assets/memories.png";
 import shoppy from "../../assets/shoppy.png";
+import krypt from "../../assets/krypt.png";
 import "./portfolio.css";
+
+const data = [
+	{
+		id: 1,
+		image: proshop,
+		title: "E-Commerce App with paypal",
+		github: "https://github.com/SagarM21/proShop",
+		demo: "https://proshop-e-commerce.herokuapp.com/",
+	},
+	{
+		id: 2,
+		image: dev,
+		title: "A webapp to connect developers",
+		github: "https://github.com/SagarM21/DevConnector",
+		demo: "https://idev-connector.herokuapp.com/",
+	},
+	{
+		id: 3,
+		image: jobify,
+		title: "Jobify - track your job status",
+		github: "https://github.com/SagarM21/Jobify",
+		demo: "https://jobify-job-tracker.herokuapp.com/landing",
+	},
+	{
+		id: 4,
+		image: memories,
+		title: "Save your beautiful Memories",
+		github: "https://github.com/SagarM21/Memories-MERN",
+		demo: "https://recall-memories.netlify.app/",
+	},
+	{
+		id: 5,
+		image: shoppy,
+		title: "React Admin Dashboard",
+		github: "https://github.com/SagarM21/Shoppy-Dashboard",
+		demo: "https://shoppy-dashboard-beta.vercel.app/",
+	},
+	{
+		id: 6,
+		image: krypt,
+		title: "Krypt - Send Crypto across the world",
+		github: "https://github.com/SagarM21/DAPP",
+		demo: "https://krypt-ether.vercel.app/",
+	},
+];
 
 const Portfolio = () => {
 	return (
@@ -13,111 +59,25 @@ const Portfolio = () => {
 			<h2>Portfolio</h2>
 
 			<div className='container portfolio__container'>
-				<article className='portfolio__item'>
-					<div className='portfolio__item-image'>
-						<img src={proshop} alt='' />
-					</div>
-					<h3>Portfolio item title</h3>
+				{data.map(({ id, title, image, github, demo }) => {
+					return (
+						<article key={id} className='portfolio__item'>
+							<div className='portfolio__item-image'>
+								<img src={image} alt={title} />
+							</div>
+							<h3>{title}</h3>
 
-					<a
-						href='https://github.com/SagarM21/proShop'
-						className='btn'
-						target='_blank'
-					>
-						GitHub
-					</a>
-					<a
-						href='https://proshop-e-commerce.herokuapp.com/'
-						className='btn btn-primary'
-						target='_blank'
-					>
-						Live Demo
-					</a>
-				</article>
-				<article className='portfolio__item'>
-					<div className='portfolio__item-image'>
-						<img src={dev} alt='' />
-					</div>
-					<h3>Portfolio item title</h3>
-
-					<a
-						href='https://github.com/SagarM21/DevConnector'
-						className='btn'
-						target='_blank'
-					>
-						GitHub
-					</a>
-					<a
-						href='https://idev-connector.herokuapp.com/'
-						className='btn btn-primary'
-						target='_blank'
-					>
-						Live Demo
-					</a>
-				</article>
-				<article className='portfolio__item'>
-					<div className='portfolio__item-image'>
-						<img src={jobify} alt='' />
-					</div>
-					<h3>Portfolio item title</h3>
-
-					<a
-						href='https://github.com/SagarM21/Jobify'
-						className='btn'
-						target='_blank'
-					>
-						GitHub
-					</a>
-					<a
-						href='https://jobify-job-tracker.herokuapp.com/landing'
-						className='btn btn-primary'
-						target='_blank'
-					>
-						Live Demo
-					</a>
-				</article>
-				<article className='portfolio__item'>
-					<div className='portfolio__item-image'>
-						<img src={memories} alt='' />
-					</div>
-					<h3>Portfolio item title</h3>
-
-					<a
-						href='https://github.com/SagarM21/Memories-MERN'
-						className='btn'
-						target='_blank'
-					>
-						GitHub
-					</a>
-					<a
-						href='https://recall-memories.netlify.app/'
-						className='btn btn-primary'
-						target='_blank'
-					>
-						Live Demo
-					</a>
-				</article>
-				<article className='portfolio__item'>
-					<div className='portfolio__item-image'>
-						<img src={shoppy} alt='' />
-					</div>
-					<h3>Portfolio item title</h3>
-
-					<a
-						href='https://github.com/SagarM21/Shoppy-Dashboard'
-						className='btn'
-						target='_blank'
-					>
-						GitHub
-					</a>
-					<a
-						href='https://shoppy-dashboard-beta.vercel.app/'
-						className='btn btn-primary'
-						target='_blank'
-					>
-						Live Demo
-					</a>
-				</article>
+							<div className='portfolio__item-cta'>
+								<a href={github} className='btn' target='_blank'>
+									GitHub
+								</a>
+								<a href={demo} className='btn btn-primary' target='_blank'>
+									Live Demo
+								</a>
+							</div>
+						</article>
+					);
+				})}
 			</div>
 		</section>
 	);
